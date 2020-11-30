0863c551f65843fe4be2327e0d88e2628fcaf3fa
Snowitty <snowitty@163.com>
2020-11-30 17:02:13 +0800

    fix

--- handlers/index.go
+++ handlers/index.go
@@ -9,7 +9,7 @@ import (
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	files := []string{"views/layout.html", "views/navbar.html", "views/index.html"}
-	templates := template.Must(template.Must(template.ParseFiles(files...)))
+	templates := template.Must(template.ParseFiles(files...))
 	threads, err := models.Threads()
 	if err != nil {
 		templates.ExecuteTemplate(w, "layout", threads)
