187a55a4f9ed9f7287b35e0a0d0d54dae80bd84e
Snowitty <snowitty@163.com>
2020-12-01 10:56:40 +0800

    logger init info danger warning

--- handlers/helper.go
+++ handlers/helper.go
@@ -4,10 +4,37 @@ import (
 	"errors"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/snowitty/chitchat/models"
 )
+
+var logger *log.Logger
+
+func init(){
+	file, err := os.OpenFile("logs/chitchat.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil{
+		log.Fatalln("Failed to open log file", err)
+	}
+	logger = log.New(file, "INFO ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func info(args ...interface{}){
+	logger.SetPrefix("INFO ")
+	logger.Println(args...)
+}
+
+func danger(args ...interface{}){
+	logger.SetPrefix("ERROR ")
+	logger.Println(args...)
+}
+
+func warning(args ...interface{}){
+	logger.SetPrefix("WARNING")
+	logger.Println(args...)
+}
 
 func session(writer http.ResponseWriter, request *http.Request) (sess models.Session, err error) {
 	cookie, err := request.Cookie("_cookie")
